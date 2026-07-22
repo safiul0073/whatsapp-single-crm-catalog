c81534cbc558e2b8b9de6a4dac970ca5a6cd00ce
safiul0073 <mdsafiul0073@gmail.com>
2026-07-22 23:23:01 +0600

    some changes

--- public/index.php
+++ public/index.php
@@ -5,6 +5,31 @@ use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
 
+$requestPath = parse_url($_SERVER['REQUEST_URI'] ?? '/', PHP_URL_PATH) ?: '/';
+
+if (str_starts_with($requestPath, '/public')) {
+    $targetPath = substr($requestPath, strlen('/public')) ?: '/';
+    $targetPath = '/'.ltrim($targetPath, '/');
+
+    if (trim($targetPath, '/') === ($_SERVER['HTTP_HOST'] ?? '')) {
+        $targetPath = '/';
+    }
+
+    $queryString = $_SERVER['QUERY_STRING'] ?? '';
+    $location = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? 'https' : 'http')
+        .'://'.($_SERVER['HTTP_HOST'] ?? 'localhost')
+        .$targetPath
+        .($queryString !== '' ? "?{$queryString}" : '');
+
+    header("Location: {$location}", true, 301);
+    exit;
+}
+
+foreach (['SCRIPT_NAME', 'PHP_SELF'] as $serverKey) {
+    if (isset($_SERVER[$serverKey])) {
+        $_SERVER[$serverKey] = str_replace('/public/index.php', '/index.php', $_SERVER[$serverKey]);
+    }
+}
 
 // Determine if the application is in maintenance mode...
 if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
